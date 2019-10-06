168fb56566b4dacfac9703e917669bce71c8b6ba
Remi Cattiau <remi@cattiau.com>
2019-10-06 17:31:19 +0200

    feature: add IAM API to read/write policies

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,12 +2,33 @@ import { suite, test } from "mocha-typescript";
 import IamPolicyOptimizer from "./index";
 import * as fs from "fs";
 import * as assert from "assert";
+import * as AWS from "aws-sdk";
+
+const GetArgv = IamPolicyOptimizer.getArgv;
+IamPolicyOptimizer.getArgv = () => {
+  return {
+    _: []
+  };
+};
 
 @suite
 class IamPolicyOptimizerTest {
+  getSamplePolicy(): string {
+    return fs
+      .readFileSync(__dirname + "/../test/policy.json")
+      .toString()
+      .trim();
+  }
+  getSampleOptimizedPolicy(): string {
+    return fs
+      .readFileSync(__dirname + "/../test/policy.optimized.json")
+      .toString()
+      .trim();
+  }
+
   @test
   complexScenario() {
-    let policy = fs.readFileSync(__dirname + "/../test/policy.json").toString();
+    let policy = this.getSamplePolicy();
     let oldSize = JSON.stringify(JSON.parse(policy)).length;
     let newPolicy: any = IamPolicyOptimizer.reduce(policy);
     let newSize = JSON.stringify(JSON.parse(newPolicy)).length;
@@ -39,5 +60,149 @@ class IamPolicyOptimizerTest {
       true,
       "Oversize message should have been displayed"
     );
+  }
+
+  comparePolicies(p1: string, p2: string) {
+    assert.equal(
+      JSON.stringify(JSON.parse(p1)),
+      JSON.stringify(JSON.parse(p2))
+    );
+  }
+
+  async cleanPolicy(iam) {
+    try {
+      let policies = (await iam
+        .listPolicies({ Scope: "Local" })
+        .promise()).Policies.filter(p => p.PolicyName === "TestPolicy");
+      if (!policies.length) {
+        return;
+      }
+      await iam.deletePolicy({ PolicyArn: policies[0].Arn }).promise();
+    } catch (err) {}
+  }
+
+  @test
+  async commandLineFile() {
+    const original = console.log;
+    let result = "";
+    console.log = (...args) => {
+      args.forEach(arg => (result += arg.toString()));
+    };
+    IamPolicyOptimizer.getArgv = () => {
+      return {
+        _: [__dirname + "/../test/policy.json"]
+      };
+    };
+    await IamPolicyOptimizer.commandLine();
+    this.comparePolicies(this.getSampleOptimizedPolicy(), result.trim());
+  }
+
+  @test
+  async commandLineFileNotFound() {
+    const original = console.log;
+    let result = "";
+    console.log = (...args) => {
+      args.forEach(arg => (result += arg.toString()));
+    };
+    IamPolicyOptimizer.getArgv = () => {
+      return {
+        _: [__dirname + "/../test/policy.nope.json"]
+      };
+    };
+    await IamPolicyOptimizer.commandLine();
+    assert.equal(result.endsWith("test/policy.nope.json was not found"), true);
+  }
+
+  @test
+  async commandLineEmpty() {
+    const original = console.log;
+    let result = "";
+    console.log = (...args) => {
+      args.forEach(arg => (result += arg.toString()));
+    };
+    IamPolicyOptimizer.getArgv = () => {
+      return {
+        _: []
+      };
+    };
+    await IamPolicyOptimizer.commandLine();
+    assert.equal(
+      result,
+      "iam-policy-generator must use either --arn or a file argument"
+    );
+  }
+
+  @test
+  async getArgv() {
+    IamPolicyOptimizer.getArgv = GetArgv;
+    process.argv = [
+      "node",
+      "iam-policy-optimizer",
+      "--arn",
+      "testor",
+      "--save"
+    ];
+    let info = IamPolicyOptimizer.getArgv();
+    assert.equal(info.save, true);
+    assert.equal(info.arn, "testor");
+  }
+
+  @test
+  async iamPolicy() {
+    const original = console.log;
+    IamPolicyOptimizer.IAMEndpoint = "http://localhost:4593";
+    process.env.AWS_DEFAULT_REGION = "us-east-1";
+    process.env.AWS_ACCESS_KEY_ID = "localstack";
+    process.env.AWS_SECRET_ACCESS_KEY = "localstack";
+    let iam = new AWS.IAM({
+      endpoint: IamPolicyOptimizer.IAMEndpoint,
+      region: "us-east-1"
+    });
+    try {
+      await this.cleanPolicy(iam);
+      let {
+        Policy: { Arn }
+      } = await iam
+        .createPolicy({
+          PolicyDocument: this.getSamplePolicy(),
+          PolicyName: "TestPolicy"
+        })
+        .promise();
+      IamPolicyOptimizer.getArgv = () => {
+        return {
+          arn: Arn
+        };
+      };
+      let result = "";
+      console.log = (...args) => {
+        args.forEach(arg => (result += arg.toString()));
+      };
+      await IamPolicyOptimizer.commandLine();
+      this.comparePolicies(this.getSampleOptimizedPolicy(), result.trim());
+      let version = (await iam.getPolicy({ PolicyArn: Arn }).promise()).Policy
+        .DefaultVersionId;
+      assert.equal(version, "v1");
+      IamPolicyOptimizer.getArgv = () => {
+        return {
+          arn: Arn,
+          save: true
+        };
+      };
+      await IamPolicyOptimizer.commandLine();
+      version = (await iam.getPolicy({ PolicyArn: Arn }).promise()).Policy
+        .DefaultVersionId;
+      assert.equal(version, "v2");
+      await IamPolicyOptimizer.commandLine();
+      version = (await iam.getPolicy({ PolicyArn: Arn }).promise()).Policy
+        .DefaultVersionId;
+      assert.equal(
+        version,
+        "v2",
+        "Version should not change as it is the same"
+      );
+    } finally {
+      // Clean policy
+      await this.cleanPolicy(iam);
+    }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
+import * as fs from "fs";
+import * as AWS from "aws-sdk";
+
 export default class IamPolicyOptimizer {
   static POLICY_LIMIT = 6144;
+  static IAMEndpoint = undefined;
+
   static reduce(policy: string): string {
-    return JSON.stringify(this.reducePolicyObject(JSON.parse(policy)));
+    return JSON.stringify(
+      this.reducePolicyObject(JSON.parse(policy)),
+      undefined,
+      2
+    );
   }
 
   static optimizeResource(arg: string[]) {
@@ -86,4 +95,79 @@ export default class IamPolicyOptimizer {
 
     return newPolicy;
   }
+
+  static getArgv() {
+    return require("yargs")
+      .option("arn", {
+        type: "string",
+        description: "Load from IAM Policy arn"
+      })
+      .option("save", {
+        type: "boolean",
+        description: "Update the IAM Policy in-place"
+      }).argv;
+  }
+
+  static async commandLine() {
+    const argv = IamPolicyOptimizer.getArgv();
+    // IAM
+    if (argv.arn) {
+      let iam = new AWS.IAM({
+        endpoint: IamPolicyOptimizer.IAMEndpoint,
+        region: "us-east-1"
+      });
+      try {
+        let { Policy } = await iam.getPolicy({ PolicyArn: argv.arn }).promise();
+        let {
+          PolicyVersion: { Document }
+        } = await iam
+          .getPolicyVersion({
+            PolicyArn: argv.arn,
+            VersionId: Policy.DefaultVersionId
+          })
+          .promise();
+        let result = IamPolicyOptimizer.reduce(Document);
+        if (argv.save) {
+          // Update in IAM
+          if (result.trim() === Document.trim()) {
+            return;
+          }
+          await iam
+            .createPolicyVersion({
+              PolicyArn: argv.arn,
+              PolicyDocument: result,
+              SetAsDefault: true
+            })
+            .promise();
+          return;
+        } else {
+          console.log(result);
+        }
+      } catch (err) {
+        console.log("Error occured", err);
+      }
+      return;
+    } else {
+      let file = argv._[0];
+      if (!file) {
+        console.log(
+          "iam-policy-generator must use either --arn or a file argument"
+        );
+        return;
+      }
+      if (file === "-") {
+        file = 0;
+      } else {
+        if (!fs.existsSync(file)) {
+          console.log(`File ${file} was not found`);
+          return;
+        }
+      }
+      console.log(IamPolicyOptimizer.reduce(fs.readFileSync(file).toString()));
+    }
+  }
+}
+
+if (require.main === module) {
+  IamPolicyOptimizer.commandLine();
 }
